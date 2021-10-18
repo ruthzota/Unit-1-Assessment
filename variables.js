70c6cf8c40b5c3832c39be7c14027affa3050233
ruthfee zota <ruthfeezota@gmail.com>
2021-10-18 14:20:14 -0400

    Initial commit

---
+++ variables.js
@@ -0,0 +1,55 @@
+//////////////////PROBLEM 1////////////////////
+/* Create a variable called 'myName' with a value that is your name as a string. */
+
+//CODE HERE 
+
+let myName = 'Ruth Zota';
+
+//////////////////PROBLEM 2////////////////////
+/* Create a variable called 'faveNum' with a value that is your favorite number. */
+
+let faveNum = 7;
+//CODE HERE
+
+//////////////////PROBLEM 3////////////////////
+/* Create a variable called 'lovesCode' and assign it a boolean value. */
+
+let name = "lovesCode";
+let lovesCode = true;
+
+//check work */
+
+if (lovesCode === true) {
+    console.log(true);
+} else {
+  console.log(false);
+} 
+//CODE HERE
+
+//////////////////PROBLEM 4////////////////////
+/*
+  Write an if-statement that checks if the faveNum variable is 
+  equal to 13. If it is, console.log('lucky 13!'). 
+  If it's not, console.log('not lucky 13').
+*/
+
+//CODE HERE
+
+let favNum = 13
+
+if (favNum === 13){
+  console.log('lucky 13!');
+} else {
+  console.log('not lucky 13')
+}
+
+//////////////////PROBLEM 5////////////////////
+/*
+  Write a for loop that prints out your faveNum 5 times.
+*/
+
+//CODE HERE
+
+for (let i = 0; i < 5; i++) {
+  console.log('faveNum')
+}

372246525c07f53f024070a1f6286c9aa06b79a1
ruthfee zota <ruthfeezota@gmail.com>
2021-10-18 14:20:50 -0400

    Initial commit

---
+++ password.js
@@ -0,0 +1,19 @@
+alert ('Welcome to the Password Validator Tool')
+alert ('Please enter a password at least 10 characters long.')
+function checkPassword(inputtxt) 
+{ 
+let password = [];
+
+if(password.vaule.length >= 10)
+{ 
+alert('Correct')
+return true;
+} 
+else { 
+alert('Fail')
+return false;
+}
+}
+
+
+
